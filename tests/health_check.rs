fdf57d774709aede4390324b679addeb37ad1301
WildCommunist <alex@ceres.ventures>
2022-12-10 23:46:48 +0600

    Chapter 3
    Choosing a database

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,13 +1,14 @@
+use reqwest::Client;
 use std::net::TcpListener;
 
 // Launch our application in the background ~somehow~
-fn spawn_app() -> String {
+fn spawn_app() -> (String, Client) {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind tcp listener");
     let port = listener.local_addr().unwrap().port();
 
     let server = zero2prod::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
-    format!("http://127.0.0.1:{}", port)
+    (format!("http://127.0.0.1:{}", port), reqwest::Client::new())
 }
 
 // `tokio::test` is the testing equivalent of `tokio::main`.
@@ -17,8 +18,7 @@ fn spawn_app() -> String {
 // `cargo expand --test health_check` (<- name of the test file)
 #[tokio::test]
 async fn health_check_works() {
-    let address = spawn_app();
-    let client = reqwest::Client::new();
+    let (address, client) = spawn_app();
 
     // Act
     let response = client
@@ -29,4 +29,50 @@ async fn health_check_works() {
     // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let (address, client) = spawn_app();
+    // Act
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    let response = client
+        .post(&format!("{}/subscriptions", address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    // Arrange
+    let (address, client) = spawn_app();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            // Additional customised error message on test failure
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
 }
