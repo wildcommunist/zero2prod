8c6e9824db52c55cfa662808773a67b8364874e9
WildCommunist <alex@ceres.ventures>
2022-12-12 20:55:49 +0600

    Chapter 4: Telemetry
    + Started on logging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-// Stopped at 3.7 (page 102)
+// Stopped at 3.7 (page 113)
 
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
@@ -7,6 +8,7 @@ use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration file");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
--- src/startup.rs
+++ src/startup.rs
@@ -1,5 +1,6 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -13,6 +14,8 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let connection = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            // Middleware
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(connection.clone())
