cca448123d3c5860c32b089d005c04cd19318e9f
WildCommunist <alex@ceres.ventures>
2022-12-12 21:39:33 +0600

    Chapter 4: Telemetry
    + Tracing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,6 +10,19 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> impl Responder {
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    );
+    tracing::info!(
+        "[{}] Adding '{}' '{}' as a new subscriber",
+        request_id,
+        form.name,
+        form.email
+    );
     match sqlx::query!(
         r#"
     INSERT INTO subscriptions (id,email,name,subscribed_at)
@@ -23,9 +36,12 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> im
     .execute(pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            tracing::info!("[{}] New subscriber has been added", request_id,);
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            tracing::error!("[{}] Failed to save new record: {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
