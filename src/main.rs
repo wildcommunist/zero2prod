3300095e22b63f426285b68e5399f38535a0cfc3
WildCommunist <alex@ceres.ventures>
2022-12-13 19:09:08 +0600

    Chapter 4: Telemetry
    + Added telemetry lib
    + Started updating tests

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,20 @@
 // Stopped at 3.7 (page 124)
 
-use sqlx::PgPool;
+use sqlx::postgres::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // Redirect all `log` events to our subscriber
-    LogTracer::init().expect("Failed to set logger.");
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    set_global_default(subscriber).expect("Failed to set subscriber");
-
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration file");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to database");
-    run(TcpListener::bind(address)?, connection_pool)?.await
+    run(TcpListener::bind(address)?, connection_pool)?.await;
+    Ok(())
 }
